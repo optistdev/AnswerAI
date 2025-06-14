3b24afefa457493a829781adaaa793cac18c7931
blaze <mattwatson0903@gmail.com>
2025-06-14 10:30:08 -0400

    update:loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import ReactLottie from 'lottie-react';
 import lottieLoading from './lottieLoading.json';
 
 const Loading = () => {
   return (
-    <div className="fixed z-[200] bg-black backdrop-blur-sm flex items-center justify-center">
+    <div className="fixed z-[200] bg-black/60 backdrop-blur-sm flex items-center justify-center w-full h-screen">
       <div className="w-[150px]">
         <ReactLottie animationData={lottieLoading} loop autoplay className="w-full" />
       </div>
