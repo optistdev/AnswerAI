b039cd666628e640671fbe1c2eae211151a3b2f9
optistdev <dskok13371@gmail.com>
2025-06-14 17:13:00 +0200

    add input

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css';
+import "./App.css";
 
-import Loading from './components/loading';
+import Loading from "./components/loading";
+import CustomizedInput from "./components/CustomInput";
 
 function App() {
   return (
     <>
-      <Loading />
-      <div>Hello World!</div>
+      <div className="h-dvh bg-background-primary">
+        {/* <Loading /> */}
+        <div className="p-10">
+          <CustomizedInput
+            width={300}
+            height={30}
+            type="password"
+            name="email"
+            required={true}
+          />
+        </div>
+        <div>Hello World!</div>
+      </div>
     </>
   );
 }
