05e7b375cd336f540e4bd01bf7864cbe6f956f1e
blaze <mattwatson0903@gmail.com>
2025-06-14 11:27:11 -0400

    create:firsbase setting

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 
 import Loading from './components/loading';
 
+import { AuthProvider } from './context/AuthContext';
+
 function App() {
   return (
-    <>
+    <AuthProvider>
       <Loading />
       <div>Hello World!</div>
-    </>
+    </AuthProvider>
   );
 }
 
