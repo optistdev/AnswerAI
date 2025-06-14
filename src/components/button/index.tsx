35c7aa0e9b4ff435a966feefb493d4e05b92292a
optistdev <dskok13371@gmail.com>
2025-06-14 19:19:08 +0200

    update button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,20 +1,27 @@
+import { type LucideIcon } from "lucide-react";
+
 interface ButtonProps {
   className: string;
   label: string;
+  icon?: LucideIcon;
   onClick: () => void;
 }
 
-const CustomizedButton = ({ className, label, onClick }: ButtonProps) => {
+const CustomizedButton = ({
+  className,
+  icon: Icon,
+  label,
+  onClick,
+}: ButtonProps) => {
   return (
-    <div className={`relative flex items-center ${className}`}>
-      <button
-        className="w-full h-full py-2 px-3 flex items-center justify-center border border-[#5a5a5a] bg-[#242424] rounded-[4px] text-white text-lg font-semibold
-        active:scale-95 transition-all duration-200"
-        onClick={onClick}
-      >
-        {label}
-      </button>
-    </div>
+    <button
+      className={`py-[7px] px-5 justify-center border-[0.67px] border-[#5a5a5a] bg-[#242424] rounded-[4px] text-white text-[16px] font-semibold
+        active:scale-95 transition-all duration-200 flex gap-2.5 items-center ${className}`}
+      onClick={onClick}
+    >
+      {!!Icon && <Icon size={18} />}
+      {label}
+    </button>
   );
 };
 
