bd70e2e118d420e135493dc5704083a22336cb60
optistdev <dskok13371@gmail.com>
2025-06-15 05:37:33 +0200

    add tab title

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,25 +1,23 @@
-import { type LucideIcon } from "lucide-react";
-
 interface ButtonProps {
   className: string;
-  label: string;
-  icon?: LucideIcon;
+  label?: string;
+  icon?: React.ReactElement;
   onClick: () => void;
 }
 
 const CustomizedButton = ({
   className,
-  icon: Icon,
+  icon,
   label,
   onClick,
 }: ButtonProps) => {
   return (
     <button
-      className={`py-[7px] px-5 justify-center border-[0.67px] border-[#5a5a5a] bg-[#242424] rounded-[4px] text-white text-[16px] font-semibold
-        active:scale-95 transition-all duration-200 flex gap-2.5 items-center ${className}`}
+      className={`cursor-pointer border-[0.67px] border-[#5a5a5a] bg-[#242424] rounded-[4px] text-white text-[16px] font-semibold
+        active:scale-95 transition-all duration-200 flex gap-2.5 items-center justify-center ${className}`}
       onClick={onClick}
     >
-      {!!Icon && <Icon size={18} />}
+      {icon}
       {label}
     </button>
   );
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import ChargingIcon from "../assets/icons/ChargingIcon";
+import CustomizedButton from "../components/button";
+import RefreshIcon from "../assets/icons/RefreshIcon";
+import UploadIcon from "../assets/icons/UploadIcon";
 
 const DashboardPage = () => {
-  return <div className="w-full"></div>;
+  const [opened, setOpened] = useState<boolean>(false);
+
+  return (
+    <div className="w-full h-[calc(100%-87px)] bg-[#161618] rounded-[5px] border-border-primary border pl-10 pr-9">
+      <div className="flex justify-between w-full pt-10 text-white">
+        <div className="flex items-center gap-[13px]">
+          <div>
+            <ChargingIcon className="text-white" />
+          </div>
+          <p className="text-[32px] tracking-normal">Charging Station</p>
+        </div>
+        <div className="flex items-center gap-3">
+          <CustomizedButton
+            className="h-10 w-10"
+            icon={<RefreshIcon className="text-description"/>}
+            onClick={() => setOpened(true)}
+          />
+          <CustomizedButton
+            label="Edit Variables"
+            className="h-10 px-2"
+            onClick={() => setOpened(true)}
+          />
+          <CustomizedButton
+            className="h-10 w-10"
+            icon={<UploadIcon className=""/>}
+            onClick={() => setOpened(true)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
