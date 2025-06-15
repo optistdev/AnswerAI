97231a7c753d25ca09ec7592e06b02e80d261fcf
optistdev <dskok13371@gmail.com>
2025-06-15 10:25:36 +0200

    fix height

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,14 +4,14 @@ import Button from "../../components/button";
 import RefreshIcon from "../../assets/icons/RefreshIcon";
 import UploadIcon from "../../assets/icons/UploadIcon";
 import SenarioResults from "./SenarioResults";
-
+import { CircleHelp } from "lucide-react";
 
 const DashboardPage = () => {
   const [opened, setOpened] = useState<boolean>(false);
 
   return (
     <div className="w-full min-h-full bg-[#161618] rounded-[5px] border-border-primary border pl-10 pr-9 pb-3">
-      <div className="md:flex justify-between w-full pt-4 md:pt-10 text-white">
+      <div className="md:flex justify-between w-full pt-4 md:pt-8 text-white">
         <div className="flex items-center justify-center gap-[13px]">
           <div>
             <ChargingIcon className="text-white text-xs md:text-lg" />
@@ -39,13 +39,15 @@ const DashboardPage = () => {
         </div>
       </div>
       <SenarioResults />
-      <div className="text-white grid grid-cols-1 xl:grid-cols-12 gap-5 mt-[30px]">
+      <div className="text-white grid grid-cols-1 xl:grid-cols-12 gap-6 mt-[30px]">
         {/* Graph Section */}
-        <div className="xl:col-span-7 w-full rounded-xl">
+        <div className="xl:col-span-7 w-full h-full rounded-xl flex flex-col">
           <div className="mb-4">
             <h2 className="text-2xl font-semibold">Graphs</h2>
           </div>
-          <div className="w-full h-[200px] md:h-[430px] bg-[#222324] rounded-[5px] border border-[#525252]">
+
+          {/* This child will take up all remaining vertical space */}
+          <div className="flex-1 w-full bg-[#222324] rounded-[5px] border border-[#525252]">
             <div className="flex justify-end pt-8 pr-12">
               <select className="bg-[#18181A80] text-white border border-neutral-600 rounded px-3 py-2 text-sm w-full sm:w-auto">
                 <option>Unsatisfied Demand %</option>
@@ -65,7 +67,7 @@ const DashboardPage = () => {
             </button>
           </div>
 
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
             {[
               {
                 label: "Infrastructure Units",
@@ -97,13 +99,15 @@ const DashboardPage = () => {
                 className="rounded-xl border border-neutral-700 p-[30px] flex flex-col justify-between bg-[#222324]"
               >
                 <div className="flex justify-between">
-                  <div>
-                    <p className="text-lg font-medium">{item.label}</p>
-                    <p className="text-description">{item.description}</p>
-                  </div>
-                  <span className="text-xs text-neutral-400">?</span>
+                  <p className="text-lg font-medium">{item.label}</p>
+                  <span className="text-xs text-neutral-400 flex justify-end">
+                    <CircleHelp />
+                  </span>
                 </div>
-                <div className="text-[32px] font-semibold flex justify-end">
+                <p className="text-description text-[12px] leading-[150%] w-2/3">
+                  {item.description}
+                </p>
+                <div className="text-[32px] font-semibold flex justify-end mt-10">
                   {item.value}
                 </div>
               </div>
@@ -114,9 +118,5 @@ const DashboardPage = () => {
     </div>
   );
 };
-
-
-
-
 
 export default DashboardPage;
