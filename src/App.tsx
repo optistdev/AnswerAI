b3f3bfbe18ccaa5f11dd6e7ec10d2f702d19268f
blaze <mattwatson0903@gmail.com>
2025-06-15 02:47:56 -0400

    update:for fixed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
-import "./App.css";
-import { BrowserRouter } from "react-router-dom";
-import { Toaster } from "sonner";
-import AppRoutes from "./routes/AppRoutes";
-import Sidebar from "./pages/layout/SideBar";
-import Header from "./pages/layout/Header";
-import { useAuth } from "./context/AuthContext";
-import { Provider } from "react-redux";
-import { store } from "./store";
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'sonner';
+import AppRoutes from './routes/AppRoutes';
+import Sidebar from './pages/layout/SideBar';
+import Header from './pages/layout/Header';
+import { useAuth } from './context/AuthContext';
+import useAppSelector from './hooks/global/useAppSelector';
 
 function App() {
   const { user } = useAuth();
+  const { isMenuOpen } = useAppSelector((state) => state.loading);
 
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Toaster richColors position="top-right" />
-        <div className="flex">
-          {user ? <Sidebar /> : <></>}
-          <div className="w-full">
+    <BrowserRouter>
+      <Toaster richColors position="top-right" />
+      <div className="flex">
+        {user ? <Sidebar /> : <></>}
+        <div className="w-full">
+          <main className={`w-full minh-[calc(100vh-87px)] ${isMenuOpen ? 'md:ml-50' : 'md:ml-20'}`}>
             <Header />
-            <main className="w-full minh-[calc(100vh-87px)]">
-              <AppRoutes />
-            </main>
-          </div>
+            <AppRoutes />
+          </main>
         </div>
-      </BrowserRouter>
-    </Provider>
+      </div>
+    </BrowserRouter>
   );
 }
 
