f7377080e527f6a16936f233f628d5f847b74a88
blaze <mattwatson0903@gmail.com>
2025-06-14 22:03:49 -0400

    update:sidebar on parent page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
 import { Toaster } from 'sonner';
 import AppRoutes from './routes/AppRoutes';
 import Sidebar from './pages/layout/SideBar';
+import { useAuth } from './context/AuthContext';
+import { useEffect } from 'react';
 
 function App() {
+  const { user } = useAuth();
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Toaster richColors position="top-right" />
-        <div className="flex">
-          <Sidebar />
+      <Toaster richColors position="top-right" />
+      <div className="flex">
+        {user ? <Sidebar /> : <></>}
+        <div className="w-full">
           <AppRoutes />
         </div>
-      </AuthProvider>
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/layout/SideBar.tsx
+++ src/pages/layout/SideBar.tsx
@@ -1,6 +1,11 @@
-import { Menu, Home, Bell, Camera, Cloud, Settings, User } from "lucide-react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Menu, User } from 'lucide-react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '../../assets/icons/HomeIcon';
+import BellIcon from '../../assets/icons/BellIcon';
+import CloudIcon from '../../assets/icons/CloudIcon';
+import RecordIcon from '../../assets/icons/RecordIcon';
+import SettingIcon from '../../assets/icons/SettingIcon';
 
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -12,7 +17,7 @@ const Sidebar = () => {
       className={`
         h-screen border-r border-[#222324] 
         flex flex-col justify-between transition-all duration-500 
-        ${isOpen ? "w-40" : "w-20"}
+        ${isOpen ? 'w-40' : 'w-20'}
       `}
     >
       {/* Top Section */}
@@ -24,47 +29,17 @@ const Sidebar = () => {
         </div>
 
         <div className="hidden sm:flex flex-col gap-10 w-full items-center">
-          <SidebarItem
-            icon={<Home className="w-5 h-5" />}
-            label="Home"
-            isOpen={isOpen}
-            path="/dashboard"
-          />
-          <SidebarItem
-            icon={<Bell className="w-5 h-5" />}
-            label="Alerts"
-            isOpen={isOpen}
-            path="/notfound"
-          />
-          <SidebarItem
-            icon={<Camera className="w-5 h-5" />}
-            label="Media"
-            isOpen={isOpen}
-            path="/notfound"
-          />
-          <SidebarItem
-            icon={<Cloud className="w-5 h-5" />}
-            label="Upload"
-            isOpen={isOpen}
-            path="/notfound"
-          />
-          <SidebarItem
-            icon={<Settings className="w-5 h-5" />}
-            label="Settings"
-            isOpen={isOpen}
-            path="/notfound"
-          />
+          <SidebarItem icon={<HomeIcon active />} label="Home" isOpen={isOpen} path="/dashboard" />
+          <SidebarItem icon={<BellIcon />} label="Alerts" isOpen={isOpen} path="/notfound" />
+          <SidebarItem icon={<RecordIcon />} label="Media" isOpen={isOpen} path="/notfound" />
+          <SidebarItem icon={<CloudIcon />} label="Upload" isOpen={isOpen} path="/notfound" />
+          <SidebarItem icon={<SettingIcon />} label="Settings" isOpen={isOpen} path="/notfound" />
         </div>
       </div>
 
       {/* Bottom Profile */}
       <div className="flex items-center justify-center mb-4">
-        <SidebarItem
-          icon={<User className="w-5 h-5" />}
-          label="Profile"
-          isOpen={isOpen}
-          path="/profile"
-        />
+        <SidebarItem icon={<User className="w-5 h-5" />} label="Profile" isOpen={isOpen} path="/profile" />
       </div>
     </div>
   );
@@ -86,7 +61,7 @@ const SidebarItem = ({
     <button
       className={`
         flex items-center gap-3 text-white text-sm w-full px-3 py-2 cursor-pointer
-        ${isOpen ? "justify-start" : "justify-center"}
+        ${isOpen ? 'justify-start' : 'justify-center'}
       `}
       onClick={() => navigate(path)}
     >
