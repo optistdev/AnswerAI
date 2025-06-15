09ffcf71c0d960d434d72424baa889961c46bd8b
optistdev <dskok13371@gmail.com>
2025-06-15 08:30:46 +0200

    fix bug

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -40,6 +40,76 @@ const DashboardPage = () => {
         </div>
       </div>
       <SenarioResults />
+      <div className="text-white grid grid-cols-1 xl:grid-cols-12 gap-5 mt-[30px]">
+        {/* Graph Section */}
+        <div className="xl:col-span-7 w-full rounded-xl">
+          <div className="mb-4">
+            <h2 className="text-2xl font-semibold">Graphs</h2>
+          </div>
+          <div className="w-full h-[200px] md:h-[430px] bg-[#222324] rounded-[5px] border border-[#525252]">
+            <div className="flex justify-end pt-8 pr-12">
+              <select className="bg-[#18181A80] text-white border border-neutral-600 rounded px-3 py-2 text-sm w-full sm:w-auto">
+                <option>Unsatisfied Demand %</option>
+              </select>
+            </div>
+          </div>
+        </div>
+
+        {/* KPI Section */}
+        <div className="xl:col-span-5 flex flex-col">
+          <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mb-4">
+            <h2 className="text-2xl font-semibold">
+              Key Performance Indicators
+            </h2>
+            <button className="flex justify-center items-center gap-1 text-[14px] border border-neutral-600 h-8 w-[102px] rounded hover:bg-neutral-700">
+              Variables <span className="text-lg">＋</span>
+            </button>
+          </div>
+
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-5">
+            {[
+              {
+                label: "Infrastructure Units",
+                description:
+                  "This describes variable two and what the shown data means.",
+                value: "€421.07",
+              },
+              {
+                label: "Charging Growth",
+                description:
+                  "This describes variable two and what the shown data means.",
+                value: "33.07",
+              },
+              {
+                label: "Localization change",
+                description:
+                  "This describes variable two and what the shown data means.",
+                value: "21.9%",
+              },
+              {
+                label: "Fleet growth",
+                description:
+                  "This describes variable two and what the shown data means.",
+                value: "7.03%",
+              },
+            ].map((item, idx) => (
+              <div
+                key={idx}
+                className="rounded-xl border border-neutral-700 p-[30px] flex flex-col justify-between bg-[#222324]"
+              >
+                <div className="flex justify-between">
+                  <div>
+                    <p className="text-lg font-medium">{item.label}</p>
+                    <p className="text-description">{item.description}</p>
+                  </div>
+                  <span className="text-xs text-neutral-400">?</span>
+                </div>
+                <div className="text-[32px] font-semibold flex justify-end">{item.value}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -52,11 +122,13 @@ const results = [
 const SenarioResults = () => {
   const [show, setShow] = useState<boolean>(true);
   return (
-    <div className="pt-4 md:pt-10 flex flex-col transition-all duration-500">
+    <div className="pt-4 md:pt-8 flex flex-col">
       <div className="flex justify-between">
         <div className="flex justify-between gap-2.5 items-center">
           <SparklesIcon className="text-[#DAFD7F]" />
-          <p className="text-xl md:text-2xl text-[#DCFF7F]">Best Scenario Results</p>
+          <p className="text-xl md:text-2xl text-[#DCFF7F]">
+            Best Scenario Results
+          </p>
         </div>
         <CustomizedButton
           className="rounded-full border-[#C8E972] border w-11 h-[34px] "
@@ -70,16 +142,26 @@ const SenarioResults = () => {
           onClick={() => setShow(!show)}
         />
       </div>
-      <div className="flex flex-col gap-2.5 pt-5">
+      <div
+        className={`flex flex-col gap-2.5 pt-5 transition-all duration-500 ease-in-out transform ${
+          show
+            ? "opacity-100 translate-y-0"
+            : "opacity-0 -translate-y-2 pointer-events-none"
+        }`}
+      >
         {show &&
           results.length &&
-          results.map((item: string, index: number) => (
+          results.map((result: string, index: number) => (
             <div
-              key={`${item}-${index}`}
+              key={`${result}-${index}`}
               className="flex justify-between items-center text-[#C8E972] border-[#C8E972] border-[0.5px] rounded-[6px] py-4 px-6"
             >
-              <p>{item}</p>
-              <CustomizedButton className="" onClick={() => console.log("")} icon={<DotsIcon className="text-[#C8E972]" />}/>
+              <p>{result}</p>
+              <CustomizedButton
+                className="border-none bg-inherit"
+                onClick={() => console.log("")}
+                icon={<DotsIcon className="text-[#C8E972]" />}
+              />
             </div>
           ))}
       </div>
@@ -88,4 +170,3 @@ const SenarioResults = () => {
 };
 
 export default DashboardPage;
-
